afa7a0bff29e90b2f9d8492526d836c1f12a5ac2
sheena849 <sheenamugo04@gmail.com>
2024-09-17 08:23:02 +0300

    Test Lab 2

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const name = "Joe";
-const height = 74;
+// const name = "Joe";
+// const height = 74;
+// const message = `${name} is ${height} inches tall`;
+
+// module.exports = {name, height, message};
+
+const name = "Susan";
+const height = 38;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
